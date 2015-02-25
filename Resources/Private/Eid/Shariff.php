a07a987b0484ba3804146e103e53a76a78b61bf9
Markus Klein <klein.t3@reelworx.at>
2015-02-25 21:00:00 +0100

    [TASK] Fix configuration
    Change-Id: I172fbf377c6e1a3b74eac6735aedac60bed73f81
    Releases: 1.0
    Reviewed-on: http://review.typo3.org/37245
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- Resources/Private/Eid/Shariff.php
+++ Resources/Private/Eid/Shariff.php
@@ -15,25 +15,44 @@ namespace Reelworx\ShariffBackend\Shariff;
 require_once __DIR__ . '/../Shariff/vendor/autoload.php';
 
 use Heise\Shariff\Backend;
+use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class Shariff {
 
 	public function render() {
-		header('Content-type: application/json');
-
-		$extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['rx_shariff']);
-		if (!is_array($extensionConfiguration)) {
-			$extensionConfiguration = array('configurationFile' => 'EXT:rx_shariff/Configuration/shariff.json');
+		$extensionConfiguration = array(
+			'services' => 'GooglePlus,Twitter,Facebook,LinkedIn,Reddit,StumbleUpon,Flattr,Pinterest,Xing',
+			'ttl' => 60,
+			'facebook_app_id' => '',
+			'facebook_secret' => ''
+		);
+		$userExtensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['rx_shariff']);
+		if (is_array($userExtensionConfiguration)) {
+			$extensionConfiguration = array_replace($extensionConfiguration, $userExtensionConfiguration);
 		}
-		$configurationFilePath = GeneralUtility::getFileAbsFileName($extensionConfiguration['configurationFile']);
-		$configurationFileContent = GeneralUtility::getUrl($configurationFilePath);
-		$configuration = json_decode($configurationFileContent, TRUE);
-		$shariff = new Backend($configuration);
+
+		$configuration = array(
+			'services' => GeneralUtility::trimExplode(',', $extensionConfiguration['services']),
+			'cache' => array(
+				'ttl' => (int)$extensionConfiguration['ttl'],
+				'cacheDir' => PATH_site . 'typo3temp',
+			),
+		);
+
+		if (in_array('Facebook', $configuration['services'], TRUE)) {
+			$configuration['Facebook'] = array(
+				'app_id' => $extensionConfiguration['facebook_app_id'],
+				'secret' => $extensionConfiguration['facebook_secret'],
+			);
+		}
 
 		$url = !empty($_GET['url']) ? $_GET['url'] : (string)$_SERVER['HTTP_REFERER'];
+
+		$shariff = new Backend($configuration);
 		$result = $shariff->get($url);
-var_dump($result);
+
+		header('Content-type: application/json');
 		echo json_encode($result);
 	}
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF['rx_shariff'] = array (
 	'title' => 'Heise Shariff Backend Module',
 	'description' => 'Shariff Backend implementation for TYPO3 CMS. Based on https://github.com/heiseonline/shariff-backend-php, Version 1.4.0',
 	'category' => 'plugin',
 	'version' => '1.0.0',
-	'state' => 'beta',
+	'state' => 'stable',
 	'uploadfolder' => 0,
 	'author' => 'Markus Klein',
 	'author_email' => 'support@reelworx.at',
 	'author_company' => 'Reelworx GmbH',
 	'constraints' => array (
 		'depends' => array (
-			'php' => '5.5.0-0.0.0',
+			'php' => '5.4.0-0.0.0',
 			'typo3' => '6.2.10-7.1.99',
 		),
 		'conflicts' => array (
