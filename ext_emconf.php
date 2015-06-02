005a5ecc78cfa0f9bcfdff854b653ff060820943
Markus Klein <markus.klein@typo3.org>
2015-06-02 16:17:54 +0200

    [RELEASE] 2.1.0
    Change-Id: Ifff76043a3667a6e27559bbc16f248d7ab340877

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,22 +2,20 @@
 
 $EM_CONF['rx_shariff'] = array (
 	'title' => 'Heise Shariff',
-	'description' => 'Shariff implementation for TYPO3 CMS including the backend module and a viewhelper.',
+	'description' => 'Shariff implementation for TYPO3 CMS including the backend module, a viewhelper and a plugin',
 	'category' => 'plugin',
-	'version' => '2.0.0',
+	'version' => '2.1.0',
 	'state' => 'stable',
-	'uploadfolder' => 0,
+	'uploadfolder' => FALSE,
 	'author' => 'Markus Klein',
 	'author_email' => 'support@reelworx.at',
 	'author_company' => 'Reelworx GmbH',
-	'constraints' => array (
-		'depends' => array (
+	'constraints' => array(
+		'depends' => array(
 			'php' => '5.4.0-0.0.0',
 			'typo3' => '6.2.9-7.2.99',
 		),
-		'conflicts' => array (
-		),
-		'suggests' => array (
-		),
+		'conflicts' => array(),
+		'suggests' => array(),
 	),
 );
