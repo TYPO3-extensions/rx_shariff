ff428b25df7e050e65034f5ff9332c1b93011253
Markus Klein <markus.klein@typo3.org>
2016-03-01 10:48:40 +0100

    [TASK] Use ::class syntax for class references
    Change-Id: I9bcec2320078b40a301c0dc3ba4c2710afcbfd56

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,15 +2,15 @@
 defined('TYPO3_MODE') || die();
 
 if (\TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('7.5')) {
-    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['shariff'] = 'Reelworx\\RxShariff\\Shariff::processRequest';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['shariff'] = \Reelworx\RxShariff\Shariff::class . '::processRequest';
 } else {
     $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['shariff'] = 'EXT:rx_shariff/Resources/Private/Eid/Shariff.php';
 }
 
 if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['rx_shariff'])) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['rx_shariff'] = [
-        'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\StringFrontend',
-        'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\SimpleFileBackend',
+        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\StringFrontend::class,
+        'backend' => \TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend::class,
         'options' => [
             'defaultLifetime' => 60,
         ],
